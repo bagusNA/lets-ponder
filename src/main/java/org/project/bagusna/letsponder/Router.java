b8575b96b2d22ae750ca176faf60ecc550028909
ReezqiAseli <cachefathur@gmail.com>
2024-05-25 03:01:01 +0800

    oh ini nah routernya

--- src/main/java/org/project/bagusna/letsponder/Router.java
+++ src/main/java/org/project/bagusna/letsponder/Router.java
@@ -1,5 +1,6 @@
 package org.project.bagusna.letsponder;
 
+import javafx.scene.control.Alert;
 import javafx.stage.Stage;
 import org.project.bagusna.letsponder.views.View;
 
@@ -9,30 +10,41 @@ public class Router {
     public static Router INSTANCE;
 
     private final Stage stage;
-    public View[] views;
-    public String currentViewName;
+    private final View[] views;
+    private String currentViewName;
 
     private Router(Stage stage, View[] views) {
         this.stage = stage;
         this.views = views;
     }
-
+    
     public static Router setup(Stage stage, View[] views) {
         INSTANCE = new Router(stage, views);
-
         return INSTANCE;
     }
-
+    
     public static Router getInstance() {
         return INSTANCE;
     }
+    
+        private void showAlert(String title, String message) {
+            Alert alert = new Alert(Alert.AlertType.ERROR);
+            alert.setTitle(title);
+            alert.setContentText(message);
+            alert.showAndWait();
+        }
 
-    public void openView(String viewName) throws IOException {
+    public void openView(String viewName) {
         setCurrentViewName(viewName);
 
-        for (View view: views) {
+        for (View view : views) {
             if (view.name.equals(currentViewName)) {
-                view.open(stage);
+                try {
+                    view.open(stage);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                    showAlert("Error", "ya error");
+                }
                 return;
             }
         }
