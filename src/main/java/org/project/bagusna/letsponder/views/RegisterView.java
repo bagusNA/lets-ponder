1428b9a396d9ab9de8dfbe75cb35791211130d62
ReezqiAseli <cachefathur@gmail.com>
2024-05-25 03:00:41 +0800

    Registroos View dengan router

---
+++ src/main/java/org/project/bagusna/letsponder/views/RegisterView.java
@@ -0,0 +1,11 @@
+package org.project.bagusna.letsponder.views;
+
+import org.project.bagusna.letsponder.controllers.LoginController;
+
+public class RegisterView extends View {
+    public RegisterView() {
+        super("Register", "register-view");
+    }
+    public void setRegisterController(LoginController LoginController) {
+    }
+}
