090d96aa9fd1fb7f1a5a0c60d5b96a5558c913a3
ReezqiAseli <cachefathur@gmail.com>
2024-05-25 03:03:35 +0800

    oh salah ini main appnya

--- src/main/java/org/project/bagusna/letsponder/LetsPonderApplication.java
+++ src/main/java/org/project/bagusna/letsponder/LetsPonderApplication.java
@@ -3,8 +3,12 @@ package org.project.bagusna.letsponder;
 import atlantafx.base.theme.PrimerDark;
 import javafx.application.Application;
 import javafx.stage.Stage;
+import org.project.bagusna.letsponder.controllers.LoginController;
+import org.project.bagusna.letsponder.services.pocketbase.PocketbaseService;
+import org.project.bagusna.letsponder.repositories.UserRepository;
 import org.project.bagusna.letsponder.views.HomeView;
 import org.project.bagusna.letsponder.views.LoginView;
+import org.project.bagusna.letsponder.views.RegisterView;
 import org.project.bagusna.letsponder.views.SearchView;
 import org.project.bagusna.letsponder.views.View;
 
@@ -15,13 +19,18 @@ public class LetsPonderApplication extends Application {
     public void start(Stage stage) throws IOException {
         Application.setUserAgentStylesheet(new PrimerDark().getUserAgentStylesheet());
 
+        PocketbaseService pocketbaseService = new PocketbaseService("https://localhost:8080");
+        UserRepository userRepository = new UserRepository();
+
         View[] views = {
                 new HomeView(),
                 new LoginView(),
+                new RegisterView(),
                 new SearchView(),
         };
 
         Router router = Router.setup(stage, views);
+
         router.openView("login");
     }
 
