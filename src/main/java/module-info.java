d037203717ebfd55dc374cc36cf34e0774c4097a
ReezqiAseli <cachefathur@gmail.com>
2024-05-25 02:58:27 +0800

    Menambahkan Export module

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -9,8 +9,12 @@ module org.project.bagusna.letsponder {
 
     opens org.project.bagusna.letsponder to javafx.fxml;
     exports org.project.bagusna.letsponder;
+    exports org.project.bagusna.letsponder.models;
     exports org.project.bagusna.letsponder.controllers;
     exports org.project.bagusna.letsponder.views;
+    exports org.project.bagusna.letsponder.services.pocketbase;
+    exports org.project.bagusna.letsponder.repositories;
+    // exports org.project.bagusna.letsponder.services.Authentication;
     opens org.project.bagusna.letsponder.controllers to javafx.fxml;
     opens org.project.bagusna.letsponder.models to com.google.gson;
 }
