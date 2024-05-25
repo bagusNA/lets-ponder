8e99d94412e80e1ab39db6cccdc291a19b648898
Bagus Nur A. <bagusnur@protonmail.com>
2024-05-25 16:06:25 +0800

    Revert "Repository dan Model User"
    This reverts commit 6e855a5fc235e466b30b588624d1b605f9966146.

--- src/main/java/org/project/bagusna/letsponder/repositories/UserRepository.java
+++ src/main/java/org/project/bagusna/letsponder/repositories/UserRepository.java
@@ -1,27 +1,10 @@
 package org.project.bagusna.letsponder.repositories;
 
 import org.project.bagusna.letsponder.models.User;
+import org.project.bagusna.letsponder.services.pocketbase.PocketbaseService;
 
-import java.util.HashMap;
-import java.util.Map;
-
-public class UserRepository {
-
-    private final Map<String, User> users;
-
-    public UserRepository() {
-        this.users = new HashMap<>();
-    }
-
-    public void save(User user) {
-        users.put(user.getUsername(), user);
-    }
-
-    public User findByUsername(String username) {
-        return users.get(username);
-    }
-
-    public boolean usernameExists(String username) {
-        return users.containsKey(username);
+public class UserRepository extends BaseRepository<User> {
+    public UserRepository(PocketbaseService pbService) {
+        super(pbService, User.collectionName, User.class);
     }
 }
