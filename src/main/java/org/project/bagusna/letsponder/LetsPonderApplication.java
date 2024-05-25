dad7fdc7c977a96a282f910c6d070a5735aa3dcb
Bagus Nur A. <bagusnur@protonmail.com>
2024-05-25 16:01:21 +0800

    Chore: cleanup

--- src/main/java/org/project/bagusna/letsponder/LetsPonderApplication.java
+++ src/main/java/org/project/bagusna/letsponder/LetsPonderApplication.java
@@ -3,23 +3,16 @@ package org.project.bagusna.letsponder;
 import atlantafx.base.theme.PrimerDark;
 import javafx.application.Application;
 import javafx.stage.Stage;
-import org.project.bagusna.letsponder.services.pocketbase.PocketbaseService;
-import org.project.bagusna.letsponder.repositories.UserRepository;
 import org.project.bagusna.letsponder.views.HomeView;
 import org.project.bagusna.letsponder.views.LoginView;
 import org.project.bagusna.letsponder.views.RegisterView;
 import org.project.bagusna.letsponder.views.SearchView;
 import org.project.bagusna.letsponder.views.View;
 
-import java.io.IOException;
-
 public class LetsPonderApplication extends Application {
     @Override
-    public void start(Stage stage) throws IOException {
+    public void start(Stage stage) {
         Application.setUserAgentStylesheet(new PrimerDark().getUserAgentStylesheet());
-
-        PocketbaseService pocketbaseService = new PocketbaseService();
-        UserRepository userRepository = new UserRepository();
 
         View[] views = {
                 new HomeView(),
