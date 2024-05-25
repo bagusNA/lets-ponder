bf8a04e5653d458e3ff06cfd9c280f0da0aeae7e
Bagus Nur A. <bagusnur@protonmail.com>
2024-05-25 23:43:10 +0800

    Feat: Search menu

--- src/main/java/org/project/bagusna/letsponder/controllers/SearchController.java
+++ src/main/java/org/project/bagusna/letsponder/controllers/SearchController.java
@@ -1,4 +1,109 @@
 package org.project.bagusna.letsponder.controllers;
 
+import javafx.fxml.FXML;
+import javafx.geometry.Pos;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.Priority;
+import javafx.scene.layout.VBox;
+import org.kordamp.ikonli.javafx.FontIcon;
+import org.project.bagusna.letsponder.Router;
+import org.project.bagusna.letsponder.models.Question;
+import org.project.bagusna.letsponder.repositories.QuestionRepository;
+
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.util.ArrayList;
+
 public class SearchController {
+    private final QuestionRepository questionRepository;
+    private final Router router;
+    private ArrayList<Question> questions;
+
+    @FXML
+    private VBox listContainer;
+    @FXML
+    private TextField searchInput;
+
+    public SearchController(QuestionRepository questionRepository) {
+        this.questionRepository = questionRepository;
+        this.router = Router.getInstance();
+    }
+
+    @FXML
+    public void initialize() {
+        this.buildList();
+    }
+
+    @FXML
+    private void onBackButtonAction() {
+        this.router.openView("home");
+    }
+
+    @FXML
+    private void searchAction() {
+        String query = this.searchInput.getText();
+
+        try {
+            this.questions = this.questionRepository.getByTitle(query).getItems();
+        } catch (URISyntaxException | IOException | InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+
+        this.buildList();
+    }
+
+    @FXML
+    private void onListItemClicked(MouseEvent event, Question question) {
+        System.out.println(question.getTitle());
+    }
+
+    private void buildList() {
+        if (this.questions == null) {
+            return;
+        }
+
+        for (Question question: this.questions) {
+            this.buildListItem(question);
+        }
+    }
+
+    private void buildListItem(Question question) {
+        HBox rootContainer = new HBox();
+        VBox statsContainer = new VBox();
+
+        Label title = new Label(question.getTitle());
+        title.setMaxWidth(Double.MAX_VALUE);
+        HBox.setHgrow(title, Priority.ALWAYS);
+
+        HBox likeContainer = new HBox();
+        FontIcon likeIcon = new FontIcon("fas-heart");
+        Label likeCount = new Label("20");
+        likeContainer.setSpacing(12);
+        likeContainer.setAlignment(Pos.CENTER_RIGHT);
+        likeContainer.getChildren().add(likeIcon);
+        likeContainer.getChildren().add(likeCount);
+
+        HBox discussionContainer = new HBox();
+        FontIcon discussionIcon = new FontIcon("far-comments");
+        Label discussionCount = new Label("20");
+        discussionContainer.setSpacing(12);
+        discussionContainer.setAlignment(Pos.CENTER_RIGHT);
+        discussionContainer.getChildren().add(discussionIcon);
+        discussionContainer.getChildren().add(discussionCount);
+
+        statsContainer.setSpacing(8);
+        statsContainer.getChildren().add(likeContainer);
+        statsContainer.getChildren().add(discussionContainer);
+
+        rootContainer.setSpacing(20);
+        rootContainer.getChildren().add(title);
+        rootContainer.getChildren().add(statsContainer);
+
+        rootContainer.setOnMouseClicked(e -> this.onListItemClicked(e, question));
+
+        this.listContainer.getChildren().add(rootContainer);
+    }
 }
--- src/main/java/org/project/bagusna/letsponder/repositories/QuestionRepository.java
+++ src/main/java/org/project/bagusna/letsponder/repositories/QuestionRepository.java
@@ -1,10 +1,23 @@
 package org.project.bagusna.letsponder.repositories;
 
+import org.project.bagusna.letsponder.models.Pagination;
 import org.project.bagusna.letsponder.models.Question;
-import org.project.bagusna.letsponder.services.pocketbase.PocketbaseService;
+import org.project.bagusna.letsponder.services.pocketbase.PocketbaseRequest;
+
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.net.http.HttpResponse;
 
 public class QuestionRepository extends BaseRepository<Question> {
     public QuestionRepository() {
         super(Question.collectionName, Question.class);
+    }
+
+    public Pagination<Question> getByTitle(String title) throws URISyntaxException, IOException, InterruptedException {
+        String filter = String.format("title~'%s'", title);
+        PocketbaseRequest req = this.getRequest().filter(filter).build();
+
+        HttpResponse<String> res = req.get();
+        return this.parsePagination(res.body());
     }
 }
