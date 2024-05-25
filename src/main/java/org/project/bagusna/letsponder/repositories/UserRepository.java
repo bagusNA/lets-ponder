a6477898fc8b5b69688e1cdd822b3762cf4802cc
Bagus Nur A. <bagusnur@protonmail.com>
2024-05-25 16:07:16 +0800

    Refactor: Adjust user repository for DI support

--- src/main/java/org/project/bagusna/letsponder/repositories/UserRepository.java
+++ src/main/java/org/project/bagusna/letsponder/repositories/UserRepository.java
@@ -4,7 +4,7 @@ import org.project.bagusna.letsponder.models.User;
 import org.project.bagusna.letsponder.services.pocketbase.PocketbaseService;
 
 public class UserRepository extends BaseRepository<User> {
-    public UserRepository(PocketbaseService pbService) {
-        super(pbService, User.collectionName, User.class);
+    public UserRepository() {
+        super(User.collectionName, User.class);
     }
 }
