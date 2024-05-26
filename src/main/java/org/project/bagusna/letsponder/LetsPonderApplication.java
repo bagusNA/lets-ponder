39a64972c23631753dc8a6a00f9f2a3a5f479a60
ReezqiAseli <cachefathur@gmail.com>
2024-05-26 16:58:46 +0800

    add logo to app window

--- src/main/java/org/project/bagusna/letsponder/LetsPonderApplication.java
+++ src/main/java/org/project/bagusna/letsponder/LetsPonderApplication.java
@@ -2,7 +2,10 @@ package org.project.bagusna.letsponder;
 
 import atlantafx.base.theme.PrimerDark;
 import javafx.application.Application;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
+
+import org.project.bagusna.letsponder.views.AnswerView;
 import org.project.bagusna.letsponder.views.HomeView;
 import org.project.bagusna.letsponder.views.LoginView;
 import org.project.bagusna.letsponder.views.RegisterView;
@@ -14,16 +17,21 @@ public class LetsPonderApplication extends Application {
     public void start(Stage stage) {
         Application.setUserAgentStylesheet(new PrimerDark().getUserAgentStylesheet());
 
+        Image icon = new Image(getClass().getResourceAsStream("img/Lp.png"));
+        stage.getIcons().add(icon);
+
+
         View[] views = {
                 new HomeView(),
                 new LoginView(),
                 new RegisterView(),
                 new SearchView(),
+                new AnswerView()
         };
 
         Router router = Router.setup(stage, views);
 
-        router.openView("login");
+        router.openView("answer");
     }
 
     public static void main(String[] args) {
