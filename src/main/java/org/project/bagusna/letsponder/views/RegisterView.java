a0cd105f71c0737178b87e62f540cb2812378ce9
ReezqiAseli <cachefathur@gmail.com>
2024-05-26 16:59:19 +0800

    change Register into register

--- src/main/java/org/project/bagusna/letsponder/views/RegisterView.java
+++ src/main/java/org/project/bagusna/letsponder/views/RegisterView.java
@@ -4,7 +4,7 @@ import org.project.bagusna.letsponder.controllers.LoginController;
 
 public class RegisterView extends View {
     public RegisterView() {
-        super("Register", "register-view");
+        super("register", "register-view");
     }
     public void setRegisterController(LoginController LoginController) {
     }
