0e392c81bfc7aac8373c572f72345c74b13a270f
Bagus Nur A. <bagusnur@protonmail.com>
2024-05-28 21:18:50 +0800

    Fix: State not persisting when changing views

--- src/main/java/org/project/bagusna/letsponder/core/Router.java
+++ src/main/java/org/project/bagusna/letsponder/core/Router.java
@@ -1,21 +1,32 @@
 package org.project.bagusna.letsponder.core;
 
-import javafx.scene.control.Alert;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
 import javafx.stage.Stage;
+import org.project.bagusna.letsponder.LetsPonderApplication;
+import org.project.bagusna.letsponder.services.Service;
 import org.project.bagusna.letsponder.views.View;
 
 import java.io.IOException;
+import java.net.URL;
+import java.util.HashMap;
 
 public class Router {
     public static Router INSTANCE;
 
     private final Stage stage;
-    private final View[] views;
+    private final HashMap<String, View> views;
+    private final HashMap<String, Scene> instanciatedScenes;
     private String currentViewName;
 
     private Router(Stage stage, View[] views) {
         this.stage = stage;
-        this.views = views;
+        this.views = new HashMap<>();
+        this.instanciatedScenes = new HashMap<>();
+
+        for (View view: views) {
+            this.views.put(view.name, view);
+        }
     }
     
     public static Router setup(Stage stage, View[] views) {
@@ -26,41 +37,41 @@ public class Router {
     public static Router getInstance() {
         return INSTANCE;
     }
-    
-        private void showAlert(String title, String message) {
-            Alert alert = new Alert(Alert.AlertType.ERROR);
-            alert.setTitle(title);
-            alert.setContentText(message);
-            alert.showAndWait();
-        }
 
     public void openView(String viewName) {
+        if (viewName.equals(this.currentViewName)) {
+            return;
+        }
+
         setCurrentViewName(viewName);
 
-        for (View view : views) {
-            if (view.name.equals(currentViewName)) {
-                try {
-                    view.open(stage);
-                } catch (IOException e) {
-                    e.printStackTrace();
-                    showAlert("Error", "ya error");
-                }
-                return;
-            }
+        Scene scene;
+        View view = this.views.get(viewName);
+
+        if (this.instanciatedScenes.containsKey(viewName)) {
+            scene = this.instanciatedScenes.get(viewName);
         }
+        else {
+            URL layoutResource = LetsPonderApplication.class.getResource(view.getFileName());
+            FXMLLoader layout = new FXMLLoader(layoutResource);
+
+            Service.inject(layout);
+
+            try {
+                scene = new Scene(layout.load(), 800, 600);
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+
+            this.instanciatedScenes.put(view.name, scene);
+        }
+
+        stage.setTitle(view.getTitle());
+        stage.setScene(scene);
+        stage.show();
     }
 
     public void setCurrentViewName(String currentViewName) {
         this.currentViewName = currentViewName;
-    }
-
-    public View getCurrentView() {
-        for (View view: views) {
-            if (view.name.equals(currentViewName)) {
-                return view;
-            }
-        }
-
-        return null;
     }
 }
--- src/main/java/org/project/bagusna/letsponder/views/View.java
+++ src/main/java/org/project/bagusna/letsponder/views/View.java
@@ -1,14 +1,5 @@
 package org.project.bagusna.letsponder.views;
 
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Scene;
-import javafx.stage.Stage;
-import org.project.bagusna.letsponder.LetsPonderApplication;
-import org.project.bagusna.letsponder.services.Service;
-
-import java.io.IOException;
-import java.net.URL;
-
 public abstract class View {
     public String title;
     public String name;
@@ -19,25 +10,20 @@ public abstract class View {
         this.templateName = templateName;
     }
 
-    public void open(Stage stage) throws IOException {
-        URL layoutResource = LetsPonderApplication.class.getResource(this.getFileName());
-        FXMLLoader layout = new FXMLLoader(layoutResource);
-
-        Service.inject(layout);
-
-        Scene scene = new Scene(layout.load(), 800, 600);
-
-        stage.setTitle(title);
-        stage.setScene(scene);
-        stage.show();
-    }
-
     public String getName() {
         return name;
     }
 
     public void setName(String name) {
         this.name = name;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
     }
 
     public String getTemplateName() {
