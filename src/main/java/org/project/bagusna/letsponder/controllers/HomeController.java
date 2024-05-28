20630c68015b5bcb45034e03cbba3f56b4bb6530
Bagus Nur A. <bagusnur@protonmail.com>
2024-05-28 20:03:00 +0800

    Feat: Threading for home controller

--- src/main/java/org/project/bagusna/letsponder/controllers/HomeController.java
+++ src/main/java/org/project/bagusna/letsponder/controllers/HomeController.java
@@ -1,5 +1,6 @@
 package org.project.bagusna.letsponder.controllers;
 
+import javafx.application.Platform;
 import javafx.fxml.FXML;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.VBox;
@@ -13,9 +14,11 @@ import org.project.bagusna.letsponder.views.components.QuestionListBlock;
 import java.io.IOException;
 import java.net.URISyntaxException;
 import java.util.ArrayList;
+import java.util.concurrent.ExecutorService;
 
 public class HomeController {
     private final Router router;
+    private final ExecutorService thread;
     private final QuestionRepository questionRepository;
     private final TopicRepository topicRepository;
     private ArrayList<Topic> topics;
@@ -26,21 +29,27 @@ public class HomeController {
     public HomeController(QuestionRepository questionRepository, TopicRepository topicRepository) {
         this.questionRepository = questionRepository;
         this.topicRepository = topicRepository;
+
         this.router = Router.getInstance();
+        this.thread = ThreadPool.getThread();
     }
 
     @FXML
     private void initialize() {
-        try {
-            this.topics = topicRepository.getAll().getItems();
-        } catch (URISyntaxException | IOException | InterruptedException e) {
-            throw new RuntimeException(e);
-        }
+        thread.execute(() -> {
+            try {
+                topics = topicRepository.getAll().getItems();
+            } catch (URISyntaxException | IOException | InterruptedException e) {
+                throw new RuntimeException(e);
+            }
 
-        for (Topic topic: this.topics) {
-            QuestionListBlock block = this.buildTopicQuestionListBlock(topic);
-            this.mainContentContainer.getChildren().add(block);
-        }
+            Platform.runLater(() -> {
+                for (Topic topic: topics) {
+                    QuestionListBlock block = buildTopicQuestionListBlock(topic);
+                    mainContentContainer.getChildren().add(block);
+                }
+            });
+        });
     }
 
     @FXML
@@ -50,8 +59,7 @@ public class HomeController {
 
     public QuestionListBlock buildTopicQuestionListBlock(Topic topic) {
         try {
-            QuestionListBlock block = new QuestionListBlock(topic.getTitle(), questionRepository.getByTopic(topic).getItems());
-            return block;
+            return new QuestionListBlock(topic.getTitle(), questionRepository.getByTopic(topic).getItems());
         } catch (URISyntaxException | IOException | InterruptedException e) {
             throw new RuntimeException(e);
         }
