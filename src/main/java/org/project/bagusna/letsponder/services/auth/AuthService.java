d73836cdd5245f688e81b9c1d74d28ff4aa14685
Bagus Nur A. <bagusnur@protonmail.com>
2024-05-29 16:43:08 +0800

    Save user on login

--- src/main/java/org/project/bagusna/letsponder/services/auth/AuthService.java
+++ src/main/java/org/project/bagusna/letsponder/services/auth/AuthService.java
@@ -1,23 +1,26 @@
 package org.project.bagusna.letsponder.services.auth;
 
+import com.google.gson.Gson;
 import org.project.bagusna.letsponder.dto.formrequests.AuthLoginFormRequest;
+import org.project.bagusna.letsponder.dto.responses.AuthSuccessResponse;
 import org.project.bagusna.letsponder.models.User;
 import org.project.bagusna.letsponder.services.pocketbase.PocketbaseRequest;
 import org.project.bagusna.letsponder.services.pocketbase.PocketbaseService;
 
 import java.io.IOException;
-import java.net.HttpURLConnection;
 import java.net.URISyntaxException;
 import java.net.http.HttpResponse;
 
 public class AuthService {
     private final PocketbaseService pocketbaseService;
+    private final Gson gson;
 
     public AuthService() {
         this.pocketbaseService = new PocketbaseService();
+        this.gson = new Gson();
     }
 
-    public boolean authenticate(String identity, String password) {
+    public AuthSuccessResponse authenticate(String identity, String password) {
         PocketbaseRequest req = this.pocketbaseService.getBuilder()
                 .collection(User.collectionName)
                 .slug("auth-with-password")
@@ -27,7 +30,8 @@ public class AuthService {
 
         try {
             HttpResponse<String> res = req.post(body);
-            return res.statusCode() == HttpURLConnection.HTTP_OK;
+
+            return this.gson.fromJson(res.body(), AuthSuccessResponse.class);
         }
         catch (URISyntaxException | IOException | InterruptedException e) {
             throw new RuntimeException(e);
