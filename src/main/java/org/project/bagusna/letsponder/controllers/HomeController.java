ed3c6c03572ced8ee133377a447c2c6806b8209e
Bagus Nur A. <bagusnur@protonmail.com>
2024-05-29 16:44:02 +0800

    Show user avatar on home

--- src/main/java/org/project/bagusna/letsponder/controllers/HomeController.java
+++ src/main/java/org/project/bagusna/letsponder/controllers/HomeController.java
@@ -3,12 +3,20 @@ package org.project.bagusna.letsponder.controllers;
 import javafx.application.Platform;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.geometry.Side;
 import javafx.scene.control.Button;
+import javafx.scene.control.ContextMenu;
+import javafx.scene.control.MenuItem;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.VBox;
+import javafx.scene.shape.Circle;
 import org.project.bagusna.letsponder.models.Topic;
+import org.project.bagusna.letsponder.models.User;
 import org.project.bagusna.letsponder.repositories.QuestionRepository;
 import org.project.bagusna.letsponder.repositories.TopicRepository;
+import org.project.bagusna.letsponder.stores.AuthStore;
 import org.project.bagusna.letsponder.views.components.QuestionListBlock;
 
 import java.io.IOException;
@@ -18,6 +26,7 @@ import java.util.ArrayList;
 public class HomeController extends Controller {
     private final QuestionRepository questionRepository;
     private final TopicRepository topicRepository;
+    private final AuthStore authStore;
     private ArrayList<Topic> topics;
 
     @FXML
@@ -29,13 +38,20 @@ public class HomeController extends Controller {
     @FXML
     private Button discoverBtn;
     @FXML
-    private Button logoutBtn;
+    private Button userBtn;
+    @FXML
+    private ContextMenu userContextMenu;
+    @FXML
+    private MenuItem logoutContextItem;
+    @FXML
+    private ImageView userAvatarImageView;
 
     public HomeController(QuestionRepository questionRepository, TopicRepository topicRepository) {
         super();
 
         this.questionRepository = questionRepository;
         this.topicRepository = topicRepository;
+        this.authStore = AuthStore.getInstance();
     }
 
     @FXML
@@ -43,7 +59,13 @@ public class HomeController extends Controller {
         this.homeBtn.setOnAction((ActionEvent event) -> this.router.openView("home"));
         this.searchBtn.setOnAction((ActionEvent event) -> this.router.openView("search"));
 //        this.discoverBtn.setOnAction((ActionEvent event) -> this.router.openView("search"));
-        this.logoutBtn.setOnAction((ActionEvent event) -> this.router.openView("login"));
+        this.userBtn.setOnAction((ActionEvent event) -> userContextMenu.show(this.userBtn, Side.TOP, 0, 0));
+        this.logoutContextItem.setOnAction((ActionEvent event) -> this.router.openView("login"));
+
+        User loggedInUser = this.authStore.get();
+        this.userBtn.setText(loggedInUser.getName());
+
+        Platform.runLater(() -> this.buildAvatarImage(loggedInUser.getProfileImageUrl()));
 
         thread.execute(() -> {
             try {
@@ -72,5 +94,16 @@ public class HomeController extends Controller {
         } catch (URISyntaxException | IOException | InterruptedException e) {
             throw new RuntimeException(e);
         }
+    }
+
+    public void buildAvatarImage(String src) {
+        double width = this.userAvatarImageView.getFitWidth();
+        double height = this.userAvatarImageView.getFitHeight();
+
+        Circle circle = new Circle(width / 2, height / 2, Math.min(width, height) / 2);
+        this.userAvatarImageView.setClip(circle);
+
+        Image avatar = new Image(src);
+        this.userAvatarImageView.setImage(avatar);
     }
 }
